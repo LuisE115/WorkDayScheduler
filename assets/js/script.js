13544e19b51b93a5daa3cb89a58626370cb8f597
kalho <luisenriquez11@hotmail.com>
2022-01-30 19:37:01 -0500

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-    //Moment.js code for current date and time
+    //Moment js library to get format of date
     const NowMoment = moment().format("MMMM Do YYYY");
     let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = NowMoment;
-    
-    
+    displayDate.innerHTML = NowMoment; 
 });
-
+// function to execute all args for each element in the class
 $(".time-div").each(function() {
+    // get current hour
     const NowTime = moment().format("HH");
+    // get id and split it to get element hour id
     var timeDiv = $(this).attr("id").split("-")[1];
-    console.log(NowTime);
-    console.log(timeDiv);
-
+    // determinates if blocks are past present of future
     if (NowTime == timeDiv) {
         $(this).css("background-color", "red");
     } else if (NowTime < timeDiv) {
@@ -21,14 +19,14 @@ $(".time-div").each(function() {
         $(this).css("background-color", "#D0D0D0");
     }
 });
-
+// event to save user input in the local storage
 $(".saveBtn").click(function (event) {
     event.preventDefault();
     var args = $(this).siblings(".time-block").val();
     var time =$(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, args);
 });
-
+// get local storage for each block 
 $("#hour-09 .time-block").val(localStorage.getItem("09"));
 $("#hour-10 .time-block").val(localStorage.getItem("10"));
 $("#hour-11 .time-block").val(localStorage.getItem("11"));
